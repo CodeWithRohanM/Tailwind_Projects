1264556df02d2aec8f2e8ce3ab0f50a158b1026b
Rohan Mote <irohanmote@gmail.com>
2023-01-10 11:10:10 +0530

    Removed Background colours and made necessary changes, Site has a complete UI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import React from "react";
 const App = () => {
 
   return <>
-    <div className="bg-green-300 flex-1" id="mainContainer">
+    <div className="flex-1" id="mainContainer">
 
 
-      <div className="container mx-auto w-full p-12 bg-cyan-300">
+      <div className="container mx-auto w-full p-12">
 
 
         <div className="h-12 flex flex-row justify-between w-full mb-20">
@@ -34,19 +34,19 @@ const App = () => {
 
 
         <section className="relative flex flex-col-reverse justify-between items-center gap-y-24 text-center 
-        sm:flex-row sm:gap-x-24 sm:text-left bg-blue-300">
+        sm:flex-row sm:gap-x-24 sm:text-left">
 
-          <div className="flex flex-col gap-y-6 s
+          <div className="flex flex-col gap-y-12
             sm:items-start 
-            flex-1 z-10 bg-red-300">
+            flex-1 z-10">
             <h1 className=" text-5xl font-bold">A Simple Bookmark Manager</h1>
 
-            <p className="text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, qui, temporibus impedit quisquam assumenda culpa possimus mollitia facilis in porro asperiores quod perferendis corporis. Perspiciatis quibusdam aut veritatis adipisci unde.</p>
+            <p className="text-gray-500 leading-relaxed text-lg">A clean and simple user interface to organize your favourite websites. Open a new browser tan and see your sites load instantly. Try it for free. </p>
 
-            <div className="flex flex-row gap-x-4 items-center justify-center w-full bg-yellow-300">
-              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-gray-500 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold flex-1">Get it on Chrome</button>
+            <div className="flex flex-row gap-x-4 items-center justify-center w-full">
+              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-gray-700 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold flex-1">Get it on Chrome</button>
 
-              <button className="bg-gray-500 hover:bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold flex-1">Get it on Firefox</button>
+              <button className="bg-gray-700 hover:bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold flex-1">Get it on Firefox</button>
             </div>
           </div>
 
@@ -64,22 +64,22 @@ const App = () => {
 
 
 
-        <div className="flex flex-col gap-y-6 flex-1 justify-center items-center text-center bg-yellow-300 mt-40 mb-36">
+        <div className="flex flex-col container mx-auto max-w-xl leading-loose gap-y-6 flex-1 justify-center items-center text-center mt-40 mb-36">
           <h1 className="text-3xl font-bold">Features</h1>
-          <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore doloremque soluta autem possimus veritatis eum, atque officiis assumenda in. Recusandae vitae, nesciunt atque voluptate eum sit placeat ea doloremque ducimus?</p>
+          <p className="text-gray-500 text-lg">Our aim is to make it quick and easy for you to access your favourite websites. Your bookmark sync between your devices so you can access them on the go.</p>
         </div>
 
 
 
 
-        <section id="feature1" className="relative flex flex-col items-center gap-y-24 sm:flex-row sm:gap-x-24 bg-red-300 text-center sm:text-left mb-36">
+        <section id="feature1" className="relative flex flex-col items-center gap-y-24 sm:flex-row sm:gap-x-24text-center sm:text-left mb-36">
           <div className="flex flex-1 z-10">
             <img src="/images/illustration-features-tab-1.png"></img>
           </div>
-          <div className="flex flex-col gap-y-6 bg-yellow-300 flex-1">
-            <h1 className="text-3xl font-bold">Bookmark in One click</h1>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam velit repellat perferendis expedita repudiandae reiciendis aliquid ea asperiores provident nulla ipsum consequuntur a saepe vero natus harum, animi adipisci id.</p>
-            <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold">Get it on Firefox</button>
+          <div className="flex flex-col gap-y-8 flex-1">
+            <h1 className="text-4xl font-bold tracking-wide">Bookmark in One click</h1>
+            <p className="text-gray-500 leading-relaxed text-lg">Organize your bookmarks however you like. Our simple drag-and-drap interface gives you complete control over how you manage your favourite sites.</p>
+            <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold">More Info</button>
           </div>
 
 
@@ -91,13 +91,13 @@ const App = () => {
 
 
 
-        <section id="feature2" className="relative flex flex-col items-center gap-y-24 sm:flex-row-reverse sm:gap-x-24 bg-red-300 text-center sm:text-left mb-36">
+        <section id="feature2" className="relative flex flex-col items-center gap-y-24 sm:flex-row-reverse sm:gap-x-24 text-center sm:text-left mb-36">
           <div className="flex flex-1 z-10">
             <img src="/images/illustration-features-tab-2.png"></img>
           </div>
-          <div className="flex flex-col gap-y-6 bg-yellow-300 flex-1">
-            <h1 className="text-4xl font-bold">Bookmark in One click</h1>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam velit repellat perferendis expedita repudiandae reiciendis aliquid ea asperiores provident nulla ipsum consequuntur a saepe vero natus harum, animi adipisci id.</p>
+          <div className="flex flex-col gap-y-6 flex-1">
+            <h1 className="text-4xl font-bold tracking-wide">Save all your favourites</h1>
+            <p className="text-gray-500 text-lg">Our easy-to-use user interface lets you save all your favourites over the internet. Just click the save button and get back to it whenever you like.</p>
             <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold">Get it on Firefox</button>
           </div>
 
@@ -109,13 +109,13 @@ const App = () => {
         </section>
 
 
-        <section id="feature1" className="relative flex flex-col items-center gap-y-24 sm:flex-row sm:gap-x-24 bg-red-300 text-center sm:text-left mb-28">
+        <section id="feature1" className="relative flex flex-col items-center gap-y-24 sm:flex-row sm:gap-x-24 text-center sm:text-left mb-52">
           <div className="flex flex-1 z-10">
             <img src="/images/illustration-features-tab-3.png"></img>
           </div>
-          <div className="flex flex-col gap-y-6 bg-yellow-300 flex-1">
-            <h1 className="text-4xl font-bold">Bookmark in One click</h1>
-            <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam velit repellat perferendis expedita repudiandae reiciendis aliquid ea asperiores provident nulla ipsum consequuntur a saepe vero natus harum, animi adipisci id.</p>
+          <div className="flex flex-col gap-y-6 flex-1">
+            <h1 className="text-4xl font-bold tracking-wide">All bookmarks at one place</h1>
+            <p className="text-gray-500 text-lg">We care for your favourites and value your time. Thats the reason we gather all your saved notes and articles over the internet at once place which makes it a lot easier to access them.</p>
             <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold">Get it on Firefox</button>
           </div>
 
@@ -128,59 +128,75 @@ const App = () => {
 
 
 
-        <div className="flex flex-col gap-y-6 flex-1 justify-center items-center text-center bg-yellow-300 mb-16">
-          <h1 className="text-3xl font-bold">Features</h1>
-          <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore doloremque soluta autem possimus veritatis eum, atque officiis assumenda in. Recusandae vitae, nesciunt atque voluptate eum sit placeat ea doloremque ducimus?</p>
+        <div className="flex flex-col gap-y-8 flex-1 container mx-auto max-w-xl justify-center items-center text-center mb-32">
+          <h1 className="text-3xl font-bold tracking-wider">Download the extension</h1>
+          <p className="text-gray-500 text-xl">We've got more browsers in the pipeline. Please do let us know if you've got a favourite you'ld like us to priorotize.</p>
         </div>
 
 
-        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-8 sm:gap-x-12 w-full justify-between mx-auto bg-red-300">
+        <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-8 sm:gap-x-12 w-full justify-between mx-auto mb-52">
 
-          <div className="rounded-md shadow-xl p-5 bg-gray-200 ">
+          <div className="rounded-md shadow-xl p-5 bg-gray-100">
 
-            <div className="flex flex-col gap-y-9 items-center bg-yellow-300">
+            <div className="flex flex-col gap-y-9 items-center">
               <div className="w-auto">
                 <img src="/images/logo-chrome.svg" ></img>
               </div>
               <h1 className="text-xl text-gray-500 font-medium">Version 3.0.1 & Above</h1>
-              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold w-full">Get it on Firefox</button>
+
+              <h1 className="border-b-black border-2 w-full border-dashed"></h1>
+
+              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold w-full">Get it on Chrome</button>
             </div>
           </div>
 
-          <div className=" rounded-md shadow-xl p-5 bg-gray-200 md:mt-6">
-            <div className="flex flex-col gap-y-9 items-center bg-yellow-300">
+          <div className=" rounded-md shadow-xl p-5 bg-gray-100 md:my-6 md:-mb-8">
+            <div className="flex flex-col gap-y-9 items-center">
               <div className="w-auto">
                 <img src="/images/logo-firefox.svg" ></img>
               </div>
               <h1 className="text-xl text-gray-500 font-medium">Version 2.0.5 & Above</h1>
+
+              <h1 className="border-b-black border-2 w-full border-dashed"></h1>
+
               <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold w-full">Get it on Firefox</button>
             </div>
           </div>
 
-          <div className="rounded-md shadow-xl p-5 bg-gray-200 md:mt-12">
-            <div className="flex flex-col gap-y-9 items-center bg-yellow-300">
+          <div className="rounded-md shadow-xl p-5 bg-gray-100 md:mt-12 md:-mb-16">
+            <div className="flex flex-col gap-y-9 items-center">
               <div className="w-auto">
                 <img src="/images/logo-opera.svg" ></img>
               </div>
               <h1 className="text-xl text-gray-500 font-medium">Version 4.2.1 & Above</h1>
-              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold w-full">Get it on Firefox</button>
+
+              <h1 className="border-b-black border-2 w-full border-dashed"></h1>
+
+              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold w-full">Get it on Opera</button>
             </div>
           </div>
         </section>
 
 
+      </div>
 
 
-        <section className="w-full py-20 bg-purple-700">
-          <div className="flex flex-col gap-y-10 text-white font-bold container mx-auto max-w-md bg-yellow-300 items-center">
+
+
+
+
+
+
+      <section className="w-full py-20 bg-purple-700">
+          <div className="flex flex-col gap-y-10 text-white font-bold container mx-auto max-w-md items-center">
             <h1 className="uppercase tracking-widest text-sm">35,000+ already joined</h1>
-            <h1 className="text-3xl tracking-wider bg-red-300 text-center">Stay up-to-date with what we are doing</h1>
+            <h1 className="text-3xl tracking-wider text-center">Stay up-to-date with what we are doing</h1>
 
-            <div className="flex flex-row gap-x-8 bg-red-300 w-full">
-              <div className="flex-1 items-center flex bg-blue-300 border-b-black border">
+            <div className="flex flex-row gap-x-8 w-full">
+              <div className="flex-1 items-center flex bg-white border-b-black border rounded-md">
                 <input type="text" placeholder="Enter your email.." className="border-none py-2 px-2 appearance-none bg-transparent text-gray-500 font-bold focus:outline-none"></input>
               </div>
-              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold ">Get it on Firefox</button>
+              <button className="bg-purple-700 shadow-xl transition ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-pruple-800 duration-300 active:scale-90 px-3 py-2 rounded-md text-white uppercase font-bold border ">Contact Us</button>
             </div>
 
           </div>
@@ -190,17 +206,19 @@ const App = () => {
 
 
 
-        <nav className="h-12 bg-gray-800">
-          <div className="flex flex-col gap-y-8 md:flex-row items-center h-full bg-yellow-300 container mx-auto max-w-4xl justify-between">
+        <nav className="h-20 bg-gray-800">
+          <div className="flex flex-col gap-y-8 md:flex-row items-center h-full container mx-auto max-w-4xl justify-between text-white uppercase text-sm">
 
-            <div className="flex flex-col gap-y-8 md:flex-row md:gap-x-12 md:items-start bg-red-300 items-center">
-              <img src="images/logo-bookmark-white.png"></img>
+            <div className="flex flex-col gap-y-8 md:flex-row md:gap-x-12 md:items-start items-center">
+              <div className="-mt-1">
+                <img src="images/logo-bookmark-white.png"></img>
+              </div>
               <h1>features</h1>
               <h1>pricing</h1>
               <h1>contact</h1>
             </div>
 
-            <div className="flex flex-row gap-x-8 bg-blue-300 cursor-pointer">
+            <div className="flex flex-row gap-x-8 cursor-pointer">
               <img src="images/logo-opera.svg" className="w-6 h-6"></img>
               <img src="images/logo-firefox.svg" className="w-6 h-6"></img>
 
@@ -209,13 +227,6 @@ const App = () => {
           </div>
 
         </nav>
-
-
-
-
-      </div>
-
-
     </div>
   </>
 };
